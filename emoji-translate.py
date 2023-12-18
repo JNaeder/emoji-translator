5b0341b74c35add0f0d8c16ba6c30f7e2717043b
NaederJ <naederj@oregonstate.edu>
2023-12-17 19:25:15 -0500

    Added symbols and spacing

--- emoji-translate.py
+++ emoji-translate.py
@@ -1,24 +1,39 @@
 import argparse
 import pyperclip
 
+symbols = {
+    "@": ":alphabet-white-at:",
+    "!": ":alphabet-white-exclamation:",
+    "#": ":alphabet-white-hash:",
+    "?": ":alphabet-white-question:"
+}
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("text")
     args = parser.parse_args()
 
-    original_message = args.text.lower()
+    if args.text.endswith(".txt"):
+        with open(args.text,  "r") as the_file:
+            original_message = the_file.read().lower()
+    else:
+        original_message = args.text.lower()
+
     output = ""
 
     for letter in original_message:
         if letter.isalpha():
             output += f":alphabet-white-{letter}:"
+        elif letter.isspace():
+            output += " " * 3
+        elif letter in symbols:
+            output += symbols[letter]
         else:
             output += letter
-        # new_letter = master_dict.get(letter, letter)
 
-    print("Translation Saved To Clipboard!")
     pyperclip.copy(output)
+    print("Translation Saved To Clipboard!")
 
 
 if __name__ == "__main__":
